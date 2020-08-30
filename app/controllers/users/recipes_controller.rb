8d731ea032f5e9bd5e7df90fa57cd2fc460fb86b
Toshiki Yoko <66193942+yokotoshiki@users.noreply.github.com>
2020-08-31 04:33:32 +0900

    recipe form update
    レシピのformの変更

--- app/controllers/users/recipes_controller.rb
+++ app/controllers/users/recipes_controller.rb
@@ -50,26 +50,12 @@ class Users::RecipesController < ApplicationController
 
   def update
     @recipe = Recipe.find(params[:id])
-    if@recipe.update(recipe_update_params)
-      recipe_ingredient_update_params["recipe_ingredients_attributes"].each do |data|
-        ingredient_params = data[1]
-        if ingredient_params["name"] && ingredient_params["quanitiy"]
-            ingredient = @recipe.recipe_ingredients.find(ingredient_params["id"])
-            ingredient.update!(ingredient_params)
-          end
-        end
-        seasoning_update_params["seasonings_attributes"].each do |data|
-          seasoning_params = data[1]
-          if seasoning_params["name"] && seasoning_params["quanitiy"]
-            seasoning = @recipe.seasonings.find(seasoning_params["id"])
-            seasoning.update!(seasoning_params)
-          end
-        end
-        @tags =  @recipe.recipe_tags.destroy_all
-        params[:recipe][:tag_ids].drop(1).each do |tag_id|
-          RecipeTag.create!(recipe_id: @recipe.id , tag_id: tag_id)
-        end
-        redirect_to users_recipe_path(@recipe), notice:"レシピを編集しました。"
+    if@recipe.update(recipe_params)
+      @tags =  @recipe.recipe_tags.destroy_all
+      params[:recipe][:tag_ids].drop(1).each do |tag_id|
+        RecipeTag.create!(recipe_id: @recipe.id , tag_id: tag_id)
+      end
+      redirect_to users_recipe_path(@recipe), notice:"レシピを編集しました。"
      else
       render "edit"
      end
@@ -100,19 +86,30 @@ class Users::RecipesController < ApplicationController
   end
 
   def recipe_params
-     params.require(:recipe).permit(:name, :image, :ingredient, :seasoning, :explanation, :time, :quanitiy, :plan, :price, :recipe_category_id, recipe_ingredients_attributes: [:name,:quanitiy], seasonings_attributes: [:name,:quanitiy])
-  end
-
-  def recipe_update_params
-     params.require(:recipe).permit(:name, :image, :explanation, :time, :quanitiy, :plan, :price, :recipe_category_id)
-  end
-
-  def recipe_ingredient_update_params
-    params.require(:recipe).permit(recipe_ingredients_attributes: [:name,:quanitiy, :id])
-  end
-
-  def seasoning_update_params
-    params.require(:recipe).permit(seasonings_attributes: [:name,:quanitiy, :id])
+     params.require(:recipe).permit(
+      :name,
+      :image,
+      :ingredient,
+      :seasoning,
+      :explanation,
+      :time,
+      :quanitiy,
+      :plan,
+      :price,
+      :recipe_category_id,
+      recipe_ingredients_attributes: [
+        :name,
+        :quanitiy,
+        :id,
+        :_destroy
+      ],
+      seasonings_attributes: [
+        :name,
+        :quanitiy,
+        :id,
+        :_destroy
+      ]
+    )
   end
 end
 
